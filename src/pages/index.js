76d58274b84a7c7bacec940cd82fa222b13cd70c
Robert Smith <67526754+therls@users.noreply.github.com>
2021-07-13 01:44:56 -0700

    Navbar now responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
-import * as React from "react"
-import Layout from "../components/Layout"
-import Navbar from "../components/Navbar"
+import * as React from "react";
+// import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
 // import 'bootstrap/dist/css/bootstrap.css';
+import { Helmet } from "react-helmet";
 
 // Styles
 
@@ -10,11 +11,18 @@ import Navbar from "../components/Navbar"
 // Markup
 const IndexPage = () => {
   return (
-    <section>
-      <Navbar/>
-      <h1>This is where the hero page will be.</h1>
-    </section>
-  )
-}
+    <div className="app">
+      <Helmet>
+        <meta charSet="utf-8" name="viewport" content="width=device-width,initial-scale=1.0"/>
+        <title>Agora</title>
+      </Helmet>
 
-export default IndexPage
+      <section>
+        <Navbar />
+        <h1>This is where the hero page will be.</h1>
+      </section>
+    </div>
+  );
+};
+
+export default IndexPage;
