843c91afd748a121b8c296f51f13c6057192f86d
Robert Smith <67526754+therls@users.noreply.github.com>
2021-07-29 17:29:48 -0700

    Started Browse page...

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,12 @@
 import * as React from "react";
 import { Helmet } from "react-helmet";
 import Layout from "../components/Layout";
+import styled from "styled-components";
+import SearchIco from "../assets/search_ico.png"
+
+const StyledH2 = styled.h2 `
+    font-size: 20px;
+`
 
 const browse = () => {
     return (
@@ -10,7 +16,19 @@ const browse = () => {
                 <title>Agora | Browse</title>
             </Helmet>
             <div>
-                <h1>Test</h1>
+                <h1>Browse</h1>
+                <StyledH2>Current Location:</StyledH2>
+                <div>
+                    <StyledH2>Filter:</StyledH2>
+                    <StyledH2>Sort by:</StyledH2>
+                    <StyledH2>Distance</StyledH2>
+                    <form class="address-search">
+                        <div id="submit-box">
+                            <a type="submit" value="Submit"><img id="search-ico" src={SearchIco} alt="search"></img></a>
+                        </div>
+                         <input id="textbox" type="text" placeholder="Enter Address Here"></input>
+                    </form>
+                </div>
             </div>
         </Layout>
     )
