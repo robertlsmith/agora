516016d716f1aa55634a5e5f7e6c88ab0a844aa3
Robert Smith <67526754+therls@users.noreply.github.com>
2021-07-30 01:14:34 -0700

    Remove CardItems component

--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,15 @@
 import React, { useContext } from "react";
 import { Button } from "react-bootstrap";
 import "../components/card.css";
-import { CardItems } from "./CardItems";
 import FarmerIco from "../assets/farmer.png"
 
 export default function Card() {
-    return (
-      <div className = "card">
-        <img id="icon" src={FarmerIco} alt="icon" class = "center"></img>
-        <h1 id="name">Name</h1>
-        <h2 id="desc">Hello, this is a description of a farmer</h2>
-        <Button id="btn" class = "center">Learn More</Button>
-      </div>
-    );
-  }
+  return (
+    <div className = "card">
+      <img id="icon" src={FarmerIco} alt="icon" class = "center"></img>
+      <h1 id="name">Name</h1>
+      <h2 id="desc">Hello, this is a description of a farmer</h2>
+      <Button id="btn" class = "center">Learn More</Button>
+    </div>
+  );
+}
